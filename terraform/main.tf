37cf6632008e2c9a633ae3d2569ab6e89c589763
MADHAV S <madhavsaravanan798@gmail.com>
2026-02-11 15:40:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "game_server" {
   instance_type = "t3.small"
   key_name      = "Madhav_s"
 
-  security_groups = [aws_security_group.game.name]
+  security_groups = [aws_security_group.game088.name]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -22,8 +22,8 @@ resource "aws_instance" "game_server" {
   }
 }
 
-resource "aws_security_group" "game" {
-  name = "game"
+resource "aws_security_group" "game088" {
+  name = "game088"
 
   ingress {
     from_port   = 22
